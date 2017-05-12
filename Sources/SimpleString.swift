e88488caf003e844a86632eade9e2f02822db733
Bojan Stefanovic <bojanstef4@gmail.com>
2017-05-11 21:36:32 -0700

    Added ranges

--- Sources/SimpleString.swift
+++ Sources/SimpleString.swift
@@ -8,17 +8,12 @@
 
 import Foundation
 
-extension String {
-    private func getIndex(from offset: Int) -> String.Index {
-        if offset < 0 {
-            assert(offset >= -self.characters.count, "Index out of range")
-            return self.index(self.endIndex, offsetBy: offset)
-        } else {
-            assert(offset < self.characters.count, "Index out of range")
-            return self.index(self.startIndex, offsetBy: offset)
-        }
-    }
+protocol SimpleString {
+    subscript(_ offset: Int) -> String { get set }
+    subscript(_ range: (Int, Int)) -> String { get set }
+}
 
+extension String: SimpleString {
     subscript(_ offset: Int) -> String {
         get {
             let index = getIndex(from: offset)
@@ -30,4 +25,39 @@ extension String {
             self.replaceSubrange(index...index, with: newValue)
         }
     }
+
+    subscript(_ range: (Int, Int)) -> String {
+        get {
+            let range = getRange(from: range)
+            return String(self.substring(with: range))
+        }
+
+        set {
+            let range = getRange(from: range)
+            self.replaceSubrange(range, with: newValue)
+        }
+    }
+}
+
+fileprivate extension String {
+    func getIndex(from offset: Int) -> String.Index {
+        if offset < 0 {
+            assert(offset >= -self.characters.count, Constants.assertMessage)
+            return self.index(self.endIndex, offsetBy: offset)
+        } else {
+            assert(offset < self.characters.count, Constants.assertMessage)
+            return self.index(self.startIndex, offsetBy: offset)
+        }
+    }
+
+    func getRange(from range: (Int, Int)) -> Range<String.Index> {
+        let lowerBound = getIndex(from: range.0)
+        let upperBound = getIndex(from: range.1)
+        let checkedBounds = (lower: lowerBound, upper: upperBound)
+        return Range<String.Index>(uncheckedBounds: checkedBounds)
+    }
+}
+
+fileprivate struct Constants {
+    static let assertMessage = "Index out of range"
 }
